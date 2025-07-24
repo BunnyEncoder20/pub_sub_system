ad9ffb290c69d2e5c6a7b07c85dbfe066d9c18cb
Varun Verma <varun.verma2024@gmail.com>
2025-07-24 23:53:53 +0530

    Created collector_app's main. Server up and running

---
+++ collector_app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, status
+
+# routes
+
+
+# CORS middleware
+from fastapi.middleware.cors import CORSMiddleware
+
+
+'''------------------------------------------------------------------'''
+
+
+# making FastAPI class instance
+app = FastAPI()
+
+# lists of alllowed origins that can reach out backend
+allowed_orgins = ['*']
+
+# Adding the CORS middleware to our app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_orgins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# root route
+@app.get("/")
+async def root():
+    return {"status_code": status.HTTP_200_OK , "msg": "Hello World. This is the Collector FastAPI App"}
+
+# include routers
+# app.include_router()  # TODO: Add router when available
