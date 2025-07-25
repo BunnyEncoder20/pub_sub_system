9a1da4c279ace0c76cc6c1275f3fcac5a563e0f4
Varun Verma <varun.verma2024@gmail.com>
2025-07-25 23:05:00 +0530

    Updated collector_app device schemas

--- collector_app/schemas/device.py
+++ collector_app/schemas/device.py
@@ -1,20 +1,25 @@
 from typing import Optional
+from datetime import datetime
 from pydantic import BaseModel, Field
 from sqlalchemy.sql.sqltypes import UUID
 
 
+
 class DeviceCreate(BaseModel):
-    device_id: str = Field(...,example='device_123')
     name: str = Field(..., exmaple='Temperature Sensor')
     type: Optional[str] = Field(None, example='Sensor')
     status: Optional[str] = Field(None, exmaple='active')
+    description: Optional[str] = Field(None, example='reports temp very 60s')
+    is_active: bool = Field(True, example='True')
 
 class DeviceResponse(BaseModel):
     id: UUID
-    device_id: str
     name: str
     type: Optional[str]
     status: Optional[str]
+    description: Optional[str]
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
 
     class Config:
         orm_mode = True
