d8e4cef6edee28c953781e999ba170f90ba8c4d0
Varun Verma <varun.verma2024@gmail.com>
2025-07-27 12:35:04 +0530

    Created location schemas

--- collector_app/schemas/location.py
+++ collector_app/schemas/location.py
@@ -1,11 +1,10 @@
-from decimal import FloatOperation
 from typing import Optional
-from pydnactic import BaseModel, Field
+from pydantic import BaseModel, Field
 from datetime import datetime
 
 class LocationCreate(BaseModel):
-    device_id: str = Field(..., exmaple='device_123')
-    latitude: float = Field(..., exmaple=12.9716)
+    device_id: str = Field(..., example='device_123')
+    latitude: float = Field(..., example=12.9716)
     longitude: float = Field(..., example=77.5946)
     timestamp: Optional[datetime] = Field(default_factory=datetime.now)
 
