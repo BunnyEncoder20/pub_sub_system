093311da4ba3fc5333f56cf6abbc5b6088529cb4
Varun Verma <varun.verma2024@gmail.com>
2025-08-05 18:22:13 +0530

    update: tasks file

--- celery_worker/app/tasks.py
+++ celery_worker/app/tasks.py
@@ -1,21 +1,17 @@
 from celery import Celery
-import os
-
 from app.db import get_db_session
 from app.models import DataItem
-from dotenv import load_dotenv
-
-load_dotenv()
+from app.config import settings
 
 celery_app = Celery(
     "worker",
-    broker=os.getenv("CELERY_BROKER_URL"),
+    broker=settings.CELERY_BROKER_URL,
 )
 
 @celery_app.task(name="app.tasks.save_to_db")
 def save_to_db(data: dict):
     session = get_db_session()
-    item = DataItem(content=str(data))
+    item = DataItem(content=data.get("content"))
     session.add(item)
     session.commit()
     session.close()
