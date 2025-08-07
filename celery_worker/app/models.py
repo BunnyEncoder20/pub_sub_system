632e4c3aceb979f3580821a9e53ea279776027c1
Varun Verma <varun.verma2024@gmail.com>
2025-08-07 20:01:36 +0530

    add: device and location models to both celery and collector apps

--- celery_worker/app/models.py
+++ celery_worker/app/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -7,3 +9,20 @@ class DataItem(Base):
     __tablename__ = "data_items"
     id = Column(Integer, primary_key=True, index=True)
     content = Column(String)
+
+class Device(Base):
+    __tablename__ = "devices"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    locations = relationship("Location", back_populates="device")
+
+class Location(Base):
+    __tablename__ = "locations"
+    id = Column(Integer, primary_key=True, index=True)
+    device_id = Column(Integer, ForeignKey("devices.id"))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+    device = relationship("Device", back_populates="locations")
